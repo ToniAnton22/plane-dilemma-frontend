dd87bb933a9ccbf75ea145d650008ea4e941ea1b
ToniAnton22 <frincuanton@yahoo.com>
2024-01-27 15:19:31 +0000

    Added safe check for "timeout" errors

--- src/routes/home/players/+page.js
+++ src/routes/home/players/+page.js
@@ -3,14 +3,19 @@ import { getItem,setItem } from '$lib/storage.js'
 export const load = ({fetch}) =>{
     const fetchPlayers = async() =>{
         let players = getItem('players')
-       
+      
         if(!players || players?.message=="Internal Error"){
+
+       
             const res = await fetch('/api/getplayers')
             players = await res.json()
 
             setItem('players',players)
         }
-   
+        if(players?.errorType == "LambdaTimeout"){
+            setTimeout(() => fetchPlayers(),3000)
+        }
+
         
         return players
     }
