347f1a841df8f58a8799026df802d20f3a4f33b9
ToniAnton22 <frincuanton@yahoo.com>
2024-02-21 16:39:31 +0000

    Modified position of back arrow.

--- src/routes/home/context/+page@.svelte
+++ src/routes/home/context/+page@.svelte
@@ -24,7 +24,7 @@
     
   </script>
  
-<a href="/home" class="absolute bottom-0 left-0 w-12 h-12 m-6 pt-2 hover:pointer z-20">
+<a href="/home" class="absolute bottom-0 right-0 w-12 h-12 m-6 pt-2 hover:pointer z-20">
     <svg aria-hidden="true" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" stroke-linecap="round" stroke-linejoin="round"></path>
     </svg>
